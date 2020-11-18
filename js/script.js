aedf00cb50ab05a1f4e066f59cb02eab8ddac804
Ageesa <71363552+Ageesa@users.noreply.github.com>
2020-11-18 16:25:45 -0500

    solution and changes upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-// Here's a test channel number, check the console!
-console.log(getRandomColorChannel())
+function newColor(){
+    var colorNow=  getRandomColorChannel()
+     document.body.style.backgroundColor = colorNow;
+  document.getElementById(`msg`).innerHTML = colorNow;
+}
 
-// 1) Built a colour string using `getRandomColorChannel()` for each of the three colour channels (R, G, B)
-
-// 2) Update user interface (`document`), setting the new colour to the body's background
-
-// 3) Use the new RGB value to update the label "rgb(###, ####, ###)" (replacing ###s with the Number values)
-
-// 4) Wrap the random colour procedure in a function named `setNewColour()`, call it from the `console` to test
+function getRandomColorChannel() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    var bgColor = `rgb(${r},${g},${b})`;
+    return bgColor
+    }
